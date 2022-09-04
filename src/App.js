1f878eed25192aa8642e740e0f87c65ade01598f
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-09-04 21:18:26 +0300

    feat: Add basic Date Table component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import DateTable from "./DateTable/DateTable";
+
+function App() {
+  return (
+    <div className="App">
+      <DateTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DateTable/DateTable.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DateTable = () => {
+  return (
+    <div>
+      <h2>Crud Table using Ant Design</h2>
+    </div>
+  );
+};
+
+export default DateTable;
