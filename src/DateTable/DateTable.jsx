46001db2caf9f548873b72a16dab6d609ff4797f
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-09-04 21:27:51 +0300

    feat: Add lodash, axios, antd and antd icons ant useState and useEffect with axios

--- src/DateTable/DateTable.jsx
+++ src/DateTable/DateTable.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table } from "antd";
 
 const DateTable = () => {
+  const [gridDate, setGridDate] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadDate();
+  }, []);
+
+  const loadDate = async () => {
+    setLoading(true);
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/comments"
+    );
+    setGridDate(response.data);
+    console.log(response);
+    setLoading(false);
+  };
+
   return (
     <div>
       <h2>Crud Table using Ant Design</h2>
